42a67f94eef44e7c4eca971355496be3d919285b
Thushar Sreenivas <Thushar-Sreenivas@users.noreply.github.com>
2020-04-19 14:25:11 +0530

    Changed CSS Styles Test

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -42,7 +42,7 @@ class Blog extends Component {
                 <Switch>
                     { this.state.auth ? <Route path="/new-post"  component={asyncNewPost} /> : null }  
                     <Route path="/posts"  component={Posts} />
-                    <Route render={() => <h1>Not found</h1>} />  
+                    <Route render={() => <h1>Not found!!</h1>} />  
                     {/* <Redirect from="/" to="/posts"/> */}
                 </Switch>
                                       
